ab2597dd1158d9b84ad2f83a48b9cd83235d5ada
Apurvata Kumar <81375992+apurvata@users.noreply.github.com>
2026-09-15 21:33:24 -0400

    Update Header.jsx

--- v1/src/components/Header.jsx
+++ v1/src/components/Header.jsx
@@ -4,12 +4,21 @@ import headerImage from '../assets/headerName.png'
 const Header = () => {
     return (
         <div>
+            <div>
+                <nav className="fixed top left -translate w- max-w z- rounded- border bg- backdrop-blur shadow-[] before:content- before:absolute before:inset- before:rounded- before:border before:border-white before:bg-gradient-">
+                    <div className="px py flex center gap font-bold">
+                        <Nav.Item className="x-item">Home</Nav.Item>
+                        <Nav.Item className="x-item">About</Nav.Item>
+                        <Nav.Item className="x-item">Projects</Nav.Item>
+                        <Nav.Link className="nav-item" href="mailto:">Contact</Nav.Link>
+                    </div>
+                </nav>
+            </div>
             <header className="">
                 <img src={headerImage} width="px" alt="Header Image" class="center-image" className="headerImage" />
             </header>
         </div>
-        
     )
 }
 
-export default Header 
+export default Header
