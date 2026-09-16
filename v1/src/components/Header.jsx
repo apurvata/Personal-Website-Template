62f6c1b3a63cbf7c0fe7355c4443fa41ca87e953
Apurvata Kumar <81375992+apurvata@users.noreply.github.com>
2026-09-15 21:26:30 -0400

    Update Header.jsx

--- v1/src/components/Header.jsx
+++ v1/src/components/Header.jsx
@@ -1,1 +1,16 @@
+import Nav from 'react-bootstrap/Nav';
+import headerImage from '../assets/headerName.png'
 
+const Header = () => {
+    return (
+        <div>
+
+            <header className="">
+                <img src={headerImage} width="px" alt="Header Image" class="center-image" className="headerImage" />
+            </header>
+        </div>
+        
+    )
+}
+
+export default Header 
