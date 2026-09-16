0fded5f9d3c6941adf0a23f6d228c2391935d1de
Apurvata Kumar <81375992+apurvata@users.noreply.github.com>
2026-09-15 21:28:25 -0400

    Update Footer.jsx

--- v1/src/components/Footer.jsx
+++ v1/src/components/Footer.jsx
@@ -1,1 +1,19 @@
+import Nav from 'react-bootstrap/Nav';
 
+const Footer = () => {
+    return (
+        <>
+        <div>
+            <p class="pt text-center font-bold text- font-">Stay tuned for updates!</p>
+            <div className="px py flex center gap font-bold">
+                <Nav.Link href="https://github.com/apurvata/Personal-Website-Template" className="nav-item">FAQ</Nav.Link>
+            </div>
+            <div class="bg flex center">
+            <p>© </p>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Footer 
